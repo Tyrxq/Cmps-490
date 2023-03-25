a937fedccb23a90737529c17538a19310e68025a
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-03-25 03:21:35 -0500

    Added map of Lafayette using leaflet

---
+++ SeniorProj/Client/wwwroot/LoadMap.js
@@ -0,0 +1,12 @@
+
+export function load_map() {
+    var map = L.map('map').setView([30.22, -92.01], 12);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return "";
+    
+    
+
+}
