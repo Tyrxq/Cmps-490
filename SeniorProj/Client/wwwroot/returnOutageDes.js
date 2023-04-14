526fc6ab68d69f9bfe04e42f842c3b7afa895ff9
brennanbodin <C00430309@louisiana.edu>
2023-04-13 21:02:58 -0500

    Merge branch 'I4-38-email-notification-functionality' of C:\Users\brenn\OneDrive - University of Louisiana Lafayette\Documents\GitHub\Cmps-490 with conflicts.

---
+++ SeniorProj/Client/wwwroot/returnOutageDes.js
@@ -0,0 +1,9 @@
+﻿let input = localStorage.getItem('input')
+
+function returnText() {
+    input = document.getElementById("outage_des").value
+    localStorage.setItem('input', input)
+    alert(input)
+}
+
+/* https://www.ceos3c.com/javascript/store-user-input-in-a-variable-with-javascript/ */
