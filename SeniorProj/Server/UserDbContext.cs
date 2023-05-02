b096d26ae3d67cb8edef1e909a965db83f2f0860
Brett Lemoine <C00414899@louisiana.edu>
2023-05-01 19:42:51 -0500

    Test fix

--- SeniorProj/Server/UserDbContext.cs
+++ SeniorProj/Server/UserDbContext.cs
@@ -5,7 +5,7 @@ namespace SeniorProj.Server;
 public class UserDbContext: DbContext
 {
     protected override void OnConfiguring(
-        DbContextOptionsBuilder options) => options.UseSqlite("Data Source=SeniorProj/Sever/users.db"); 
+        DbContextOptionsBuilder options) => options.UseSqlite("Data Source=users.db"); 
     //Need to update to correct connection string and test once DB is connected
     public DbSet<User>? Users { get; set; }
     public DbSet<Outage>? Outages { get; set; }
